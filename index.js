136fc54b44f7a150e7287c3896af4a736f7d5f6f
Mahin Ahmed <mahindipto18@gmail.com>
2023-12-25 21:26:04 +0600

    added update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const  cors = require('cors');
 const app = express();
 require ("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
@@ -29,17 +29,35 @@ async function run() {
     const tasksCollection = client.db('SmartTaskDB').collection('tasks');
 
     app.get('/tasks', async (req, res) => {
-        const cursor = tasksCollection.find();
+        console.log(req.query.email)
+        let query = {};
+        if (req.query?.email) {
+          query = { email: req.query.email }
+        }
+        const cursor = await tasksCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
       })
-  
+    
+      app.get('/tasks/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await tasksCollection.findOne(query);
+        res.send(result);
+      })
 
     app.post('/tasks', async (req, res) => {
         const task = req.body
         const result = await tasksCollection.insertOne(task)
         res.send(result)
       });
+
+      app.delete('/tasks/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await tasksCollection.deleteOne(query);
+        res.send(result);
+      })
 
 
 
